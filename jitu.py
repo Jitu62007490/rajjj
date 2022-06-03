0fbaceec4f1d989752fecddef586fd0e9f0435dc
Jitu62007490 <100823962+Jitu62007490@users.noreply.github.com>
2022-06-04 00:20:26 +0530

    Add files via upload

---
+++ jitu.py
@@ -0,0 +1,164 @@
+print('''  
+             #########  ###########   ##############  #          #
+                 #           #              #         #          #
+           #     #           #              #         #          #
+           #     #           #              #         #          #
+           #     #           #              #         #          #
+            ######      ###########         #         ############                                      ''')
+
+no=int(input("Enter your no with country code +"))
+n={
+    916200749026:'''
+ >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>      
+    Name: jitendra kumar
+    Father name: Goverdhan dangi
+    Hobby: Hacking
+    Address: State>jharkhand/distick>chatra/village>sinduwari
+    Study in: (BCA) 3rd sem from chatra collage chatra
+    Girl friend: no
+    Live location: charu
+    gender:male
+    Adhar-no: 898 3984 3908
+    Facebook id:49864985699
+    Facebook password:jitu#9211
+    Age: 21 year old
+  
+ >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>      
+     ''',
+    917677533452:'''
+     >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>  
+                    Name:Ravi kumar
+                    Father name: muneshwar prashad
+                    Address: State>jharkhand/distick>chatra/village>pitiz
+                    Best Friend name: jitendra kumar
+                    Girl friend: no
+                    Age: 20
+                    Study in: (BCA) 3rd sem from chatra collage chatra
+                    Hobby: software Engineer
+                    Live location: chatra
+                    Gender: male
+                    Face colour: white
+                    Class Faind Nane: jitendra kumar
+
+         >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>              
+                                        ''',
+     919142605361:'''
+      >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> 
+
+                    Name: Abhay kumar dangi
+                    Age: around 20
+                    Address: Address: State>jharkhand/distick>chatra/village>barishakhi
+                    Gender:male
+                    Girlfriend: yes
+                    Study in: chatra collage chatra(political science)
+                    Hobby: painting
+                    Face colour: white
+                    Class Faind Nane: jitendra kumar
+
+
+         >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>             
+                    ''',
+                    916287858004:'''
+         >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>       
+
+                    Name: Rani kumari
+                    Address: State>jharkhand/distick>chatra/village>pitiz
+                    Face colour: white
+                    Gender: female
+                    Hobby: Hacking
+                    Live location: pitiz
+                    Age: around 19 years old
+                    Class Faind Nane: jitendra kumar
+
+
+         >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>              
+
+
+                    ''',
+                    91823594118:'''
+        >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>            
+       
+                    Name: Manish kumari
+                    Address: State>jharkhand/distick>chatra/village>pitiz
+                    Face colour: white
+                    Gender: female
+                    Live location: pitiz
+                    Study in: (BCA) 3rd sem from chatra collage chatra
+                    Age: around 19 years old
+                    Hobby in : computer
+                    Class Faind Nane: jitendra kumar
+
+
+        <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<            ''',
+        917667365616:'''
+         >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> 
+
+         Name: Sani kumar
+         Address: State>jharkhand/distick>chatra/village>chatra
+         Study in: (BCA) 3rd sem from chatra collage chatra
+         Age: around 20 years old
+         Hobby in : computer
+         Face colour: white
+         Gender: male
+         Class Faind Nane: jitendra kumar
+
+ >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>        
+
+         
+         ''',
+         918521366521:'''
+   <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+   Name: Rohit kumar dangi   
+   Study in: (BCA) 3rd sem from chatra collage chatra
+   Live location: chatra
+   Age: around 20 years old
+   Hobby in : computer
+   Face colour: white
+   Gender: male
+   Class Faind Nane: jitendra kuma  
+>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+         ''',
+         919431776704:'''
+<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+   Name: Shathyarthi Gyan Prashad
+   Address: State>jharkhand/distick>chatra/village>chatra
+   Live location: chatra
+   Best student: jitendra kumar
+   Working place: Chatra collage chatra(BCA department)
+   Age: around 26 years old
+   Hobby in : computer
+   Face colour: white
+   Gender: male
+
+         ''',
+         917480009395:'''
+>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+   Name: Sager kumar       
+   Study in: (BCA) 2rd sem from chatra collage chatra
+   Live location: 
+   Hobby in: computer(Graphics Dgining)
+   Face colour: white
+   Gender: male
+   Class Faind Nane: jitendra kumar
+   Age: around 21 years old     
+         
+         '''                         
+}
+if no in n:
+    i=n[no]
+    print(i)
+else:
+    print("Ooof Your No Is Wrong Try Againe")   
+
+
+
+
+
+
+
+
+
+
+
+
